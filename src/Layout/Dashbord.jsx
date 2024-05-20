409e5fca57b56ae051f4c02fdc76f661ee39434c
masud <masudranaacb@gmail.com>
2024-05-20 15:45:10 +0600

    added dashbord design and carts route

--- src/Layout/Dashbord.jsx
+++ src/Layout/Dashbord.jsx
@@ -1,8 +1,10 @@
 import { FaHome } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 
 const Dashbord = () => {
+    const {carts}=useCart()
     return (
         <div className="flex container mx-auto">
             <div className="min-h-screen bg-orange-300 w-1/5 p-4">
@@ -10,9 +12,14 @@ const Dashbord = () => {
                     <li ><NavLink className="flex gap-2 items-center" to="/dashbord"><FaHome></FaHome> User Home</NavLink></li>
                     <li><NavLink className="flex gap-2 items-center" to="/dashbord/reservation"><FaHome></FaHome> Resevation</NavLink></li>
                     <li><NavLink className="flex gap-2 items-center"><FaHome></FaHome> Payment Histroy</NavLink></li>
-                    <li><NavLink className="flex gap-2 items-center" to="/dashbord/mycarts"><FaHome></FaHome> My Cart</NavLink></li>
+                    <li><NavLink className="flex gap-2 items-center" to="/dashbord/mycarts"><FaHome></FaHome> My Cart ({carts.length})</NavLink></li>
                     <li><NavLink className="flex gap-2 items-center"><FaHome></FaHome> Add Review</NavLink></li>
                     <li><NavLink className="flex gap-2 items-center"><FaHome></FaHome> My Booking</NavLink></li>
+                    <div className="divider">OR</div>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/menu">Menu</NavLink></li>
+                    <li><NavLink to="/orders/soup">Orders</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
                 </ul>
             </div>
             <div className="flex-1 px-4">
--- src/componets/Cart.jsx
+++ src/componets/Cart.jsx
@@ -1,10 +1,38 @@
+import Swal from "sweetalert2";
 import useCart from "../hooks/useCart";
+import { MdDelete } from "react-icons/md";
+import useAxiosSecure from "../hooks/useAxiosSecure";
 
 const Cart = () => {
-    const { carts } = useCart()
+    const { carts, refetch } = useCart()
+    const AxiosSecure = useAxiosSecure()
     const totalPrice = carts.reduce((total, item) => total + item.price, 0)
-    console.log(totalPrice)
-    console.log(carts)
+    const handelDeleteItem = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                AxiosSecure.delete(`/deleteCart/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                        }
+                    })
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+            }
+        });
+    }
+
     return (
         <div>
             <div className="flex justify-between text-3xl mt-3">
@@ -45,7 +73,7 @@ const Cart = () => {
                                         <p>{item.price}</p>
                                     </td>
                                     <th>
-                                        <button className="btn btn-ghost btn-xs">details</button>
+                                        <button onClick={() => handelDeleteItem(item._id)} className="btn btn-ghost"><MdDelete className="text-xl text-red-500"></MdDelete></button>
                                     </th>
                                 </tr>)
                             }
